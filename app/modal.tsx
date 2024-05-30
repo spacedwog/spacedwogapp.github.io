b54003fe23762de00f38fb1b7fd661750ec7e4ef
Spacedwog <owner@spacedwog.com.br>
2024-05-30 17:12:15 -0300

    Commited

--- app/modal.tsx
+++ app/modal.tsx
@@ -3,27 +3,6 @@ import { Platform, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-
-function MyForm() {
-  const [name, setName] = useState("");
-
-  return (
-    <form>
-      <label>Enter your name:
-        <input
-          type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-    </form>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
 
 export default function ModalScreen() {
   return (
