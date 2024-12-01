494fbf07f24c7e8113e01d5c146e9555848615f9
Spacedwog <152028848+spacedwog@users.noreply.github.com>
2024-12-01 14:43:51 +0000

    Commited

--- app/+html.tsx
+++ app/+html.tsx
@@ -18,8 +18,6 @@ export default function Root({ children }: { children: React.ReactNode }) {
         */}
         <ScrollViewStyleReset />
 
-        <link href=""/>
-
         {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
         <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
         {/* Add any additional <head> elements that you want globally available on web... */}
